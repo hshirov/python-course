afa905874d26c0479b74ed98351d4f5087a88011
Hristo Shirov <hristo.shirov@mentormate.com>
2024-07-26 00:22:07 +0300

    Add poll detail template

--- django-basics/django-tutorial/mysite/polls/views.py
+++ django-basics/django-tutorial/mysite/polls/views.py
@@ -1,15 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    context = {"latest_questions": latest_questions}
-    return render(request, "polls/index.html", context)
+    return render(request, "polls/index.html", {"latest_questions": latest_questions})
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question %s." % question_id)
