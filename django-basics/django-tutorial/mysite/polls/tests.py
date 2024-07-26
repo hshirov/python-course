1471f64fc14ea88b6fd617ee8dc757bcb519b143
Hristo Shirov <hristo.shirov@mentormate.com>
2024-07-26 19:43:59 +0300

    Add question model tests

--- django-basics/django-tutorial/mysite/polls/tests.py
+++ django-basics/django-tutorial/mysite/polls/tests.py
@@ -1,3 +1,34 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_old_date(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        old_date = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=old_date)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_date(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        recent_date = timezone.now() - datetime.timedelta(hours=23, minutes=59)
+        recent_question = Question(pub_date=recent_date)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+    def test_was_published_recently_with_future_date(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        future_date = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=future_date)
+        self.assertIs(future_question.was_published_recently(), False)
