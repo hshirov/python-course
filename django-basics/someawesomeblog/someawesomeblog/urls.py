ca63213f453f35462782dcec7df0086da3fa0519
Hristo Shirov <hristo.shirov@mentormate.com>
2024-07-28 18:07:28 +0300

    Add logout button to home

--- django-basics/someawesomeblog/someawesomeblog/urls.py
+++ django-basics/someawesomeblog/someawesomeblog/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('userauth.urls')),
     path('', include('blog.urls')),
-    path('', include('django.contrib.auth.urls')),
+    path('auth/', include('userauth.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
