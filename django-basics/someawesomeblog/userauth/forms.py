e0900664d114f94f52de81f63851289b6f526ace
Hristo Shirov <hristo.shirov@mentormate.com>
2024-07-29 00:25:25 +0300

    Customize and add styles to login and sign up pages

--- django-basics/someawesomeblog/userauth/forms.py
+++ django-basics/someawesomeblog/userauth/forms.py
@@ -1,11 +1,25 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True, help_text='Required. Enter a valid email address.')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm Password'})
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class AuthForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
+        self.fields['password'].widget.attrs.update({'placeholder': 'Password'})
--- django-basics/someawesomeblog/userauth/views.py
+++ django-basics/someawesomeblog/userauth/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.forms import AuthenticationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, AuthForm
 from .decorators import anonymous_required
 
 
@@ -20,11 +19,11 @@ def signup(request):
 @anonymous_required
 def login(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = AuthForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             auth_login(request, user)
             return redirect('home')
     else:
-        form = AuthenticationForm()
+        form = AuthForm()
     return render(request, 'userauth/login.html', {'form': form})
