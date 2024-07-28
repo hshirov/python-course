0c7cf8d6e33453e82bf1ae7acb0bf78e65ea16c4
Hristo Shirov <hristo.shirov@mentormate.com>
2024-07-28 17:43:37 +0300

    Add minimal auth setup

---
+++ django-basics/someawesomeblog/userauth/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'userauth/signup.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'userauth/login.html', {'form': form})
