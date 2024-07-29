187f9d81fbf365ac05f89ae4622139b5cf3b9c50
Hristo Shirov <hristo.shirov@mentormate.com>
2024-07-29 12:33:06 +0300

    Add blog models

--- django-basics/someawesomeblog/blog/models.py
+++ django-basics/someawesomeblog/blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text_content = models.CharField(max_length=1000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text_content = models.CharField(max_length=280)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+
+
+class Reaction(models.Model):
+    REACTION_CHOICES = [
+        ('thumbs_up', 'Thumbs Up'),
+        ('thumbs_down', 'Thumbs Down'),
+    ]
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    reaction_type = models.CharField(max_length=11, choices=REACTION_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.reaction_type} by {self.author.username} on {self.post.title}'
