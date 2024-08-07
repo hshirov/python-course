4bc86db64ff2f2817508b88e1a5f9a2031346430
Hristo Shirov <hristo.shirov@mentormate.com>
2024-08-07 03:17:18 +0300

    Add filter by hot

--- django-basics/someawesomeblog-rest-api/blog/filters.py
+++ django-basics/someawesomeblog-rest-api/blog/filters.py
@@ -1,10 +1,21 @@
+from django.db.models import Count, Q
 import django_filters
 from .models import Post
 
 
 class PostFilter(django_filters.FilterSet):
     hashtag = django_filters.CharFilter(field_name='hashtags__name', lookup_expr='icontains')
+    hot = django_filters.BooleanFilter(label='hot', method='filter_hot')
+
+    def filter_hot(self, queryset, name, value):
+        if value:
+            posts = queryset.annotate(
+                thumbs_up_count=Count('reactions', filter=Q(reactions__reaction_type='thumbs_up')),
+                comments_count=Count('comments')
+            ).filter(thumbs_up_count__gte=5, comments_count__gte=2)
+            return posts
+        return queryset.all()
 
     class Meta:
         model = Post
-        fields = ['hashtag']
+        fields = ['hashtag', 'hot']
