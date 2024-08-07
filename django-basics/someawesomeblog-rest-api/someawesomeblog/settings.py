9d7146621e0f042616d06e76f78f731c634e2e1f
Hristo Shirov <hristo.shirov@mentormate.com>
2024-08-07 16:11:48 +0300

    Remove default filter backends from settings

--- django-basics/someawesomeblog-rest-api/someawesomeblog/settings.py
+++ django-basics/someawesomeblog-rest-api/someawesomeblog/settings.py
@@ -49,11 +49,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-        'rest_framework.filters.OrderingFilter',
-        'blog.filters.PostSearchFilter',
-    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
