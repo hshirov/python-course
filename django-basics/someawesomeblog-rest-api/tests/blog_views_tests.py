53e6be18c1633ea9a56d63bd67e825c719248a5c
Hristo Shirov <hristo.shirov@mentormate.com>
2024-08-13 14:14:46 +0300

    Add update and delete post tests

--- django-basics/someawesomeblog-rest-api/tests/blog_views_tests.py
+++ django-basics/someawesomeblog-rest-api/tests/blog_views_tests.py
@@ -1,4 +1,5 @@
 import pytest
+from django.urls import reverse
 from django.contrib.auth.models import User
 from rest_framework import status
 from blog.models import Post
@@ -6,6 +7,8 @@ from blog.models import Post
 
 @pytest.mark.django_db
 class TestPostView:
+    post_list_url = reverse('post-list')
+
     @pytest.fixture
     def create_post(self):
         def make_post(**kwargs):
@@ -17,7 +20,7 @@ class TestPostView:
         create_post(title='Test Post 1', text_content='Text', author=user)
         create_post(title='Test Post 2', text_content='Text', author=user)
 
-        response = client.get('/posts/')
+        response = client.get(self.post_list_url)
         count = response.data.get('count')
         results = response.data.get('results')
 
@@ -27,7 +30,7 @@ class TestPostView:
     
     def test_unauthenticated_user_cannot_create_post(self, api_client):
         data = {'title': 'Test Post', 'content': 'Test Content'}
-        response = api_client.post('/posts/', data)
+        response = api_client.post(self.post_list_url, data)
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
     
@@ -36,7 +39,59 @@ class TestPostView:
         api_client.force_authenticate(user=user)
 
         data = {'title': 'Test Post', 'text_content': 'Test Content'}
-        response = api_client.post('/posts/', data)
+        response = api_client.post(self.post_list_url, data)
 
         assert response.status_code == status.HTTP_201_CREATED
         assert Post.objects.count() == 1
+    
+    def test_user_can_update_own_post(self, api_client, create_post):
+        user = User.objects.create_user(username='testuser', password='password')
+        post = create_post(title='Test Post 1', text_content='Text', author=user)
+
+        api_client.force_authenticate(user=user)
+        url = reverse('post-detail', args=[post.id])
+        data = {'title': 'New Title', 'text_content': 'New Content'}
+        response = api_client.put(url, data)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert Post.objects.get(id=post.id).title == 'New Title'
+        assert Post.objects.get(id=post.id).text_content == 'New Content'
+    
+    def test_user_can_delete_own_post(self, api_client, create_post):
+        user = User.objects.create_user(username='testuser', password='password')
+        post = create_post(title='Test Post 1', text_content='Text', author=user)
+
+        api_client.force_authenticate(user=user)
+        url = reverse('post-detail', args=[post.id])
+        response = api_client.delete(url)
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert not Post.objects.filter(id=post.id).exists()
+    
+    def test_user_cannot_update_not_own_post(self, api_client, create_post):
+        author_user = User.objects.create_user(username='testuser', password='password')
+        post = create_post(title='Old Title', text_content='Old Content', author=author_user)
+
+        not_author_user = User.objects.create_user(username='testuser2', password='password')
+        api_client.force_authenticate(user=not_author_user)
+
+        url = reverse('post-detail', args=[post.id])
+        data = {'title': 'New Title', 'text_content': 'New Content'}
+        response = api_client.put(url, data)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert Post.objects.get(id=post.id).title == 'Old Title'
+        assert Post.objects.get(id=post.id).text_content == 'Old Content'
+    
+    def test_user_cannot_delete_not_own_post(self, api_client, create_post):
+        author_user = User.objects.create_user(username='testuser', password='password')
+        post = create_post(title='Test Post 1', text_content='Text', author=author_user)
+
+        not_author_user = User.objects.create_user(username='testuser2', password='password')
+        api_client.force_authenticate(user=not_author_user)
+
+        url = reverse('post-detail', args=[post.id])
+        response = api_client.delete(url)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert Post.objects.filter(id=post.id).exists()
