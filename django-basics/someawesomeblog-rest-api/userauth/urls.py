945007dc11aa494e63816cbbf48a108c25c18ae3
Hristo Shirov <hristo.shirov@mentormate.com>
2024-08-15 04:29:42 +0300

    Add get token endpoint

--- django-basics/someawesomeblog-rest-api/userauth/urls.py
+++ django-basics/someawesomeblog-rest-api/userauth/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.authtoken import views
 from .views import AuthRootView, CreateUserView, LoginView
 
 urlpatterns = [
     path('', AuthRootView.as_view()),
     path('login/', LoginView.as_view(), name='login'),
     path('signup/', CreateUserView.as_view(), name='signup'),
+    path('token/', views.obtain_auth_token)
 ]
